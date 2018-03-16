9569480c6054cfd04863ad3e10c5cf7868a02a6a
John Korsnes <john.korsnes@ice.net>
2018-03-16 13:09:46 +0100

    Sample failing w/ 2.0.6 runtime

---
+++ src/classlib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace classlib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            SqlConnectionStringBuilder connectionInfo = new SqlConnectionStringBuilder("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=Stuff;");
+        }
+    }
+
+
+}
