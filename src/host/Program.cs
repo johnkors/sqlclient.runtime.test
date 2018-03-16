45b59d0243cd87104e9115b6fefbdb5b2c3ee360
John Korsnes <john.korsnes@ice.net>
2018-03-16 15:09:30 +0100

    Removes uncommented text

--- src/host/Program.cs
+++ src/host/Program.cs
@@ -33,10 +33,5 @@ namespace sqlclient.runtime.test
             }
             
         }
-
-        // public static IWebHost BuildWebHost(string[] args) =>
-        //     WebHost.CreateDefaultBuilder(args)
-        //         .UseStartup<Startup>()
-        //         .Build();
     }
 }
