12ce63a1e78dc386246ec111e43955715f48143c
John Korsnes <john.korsnes@ice.net>
2018-03-16 14:22:22 +0100

    Smaller sample

--- src/host/Startup.cs
+++ src/host/Startup.cs
@@ -16,34 +16,12 @@ using FluentMigrator.Runner.Initialization;
 namespace sqlclient.runtime.test
 {
     public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    {     
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var connectionInfo = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PaymentLocal;Integrated Security=True;";
-            //var connectionInfo = "Data Source=someserver\\MSSQLLocalDB;Initial Catalog=PaymentLocal;Integrated Security=True;";
-            MigrateDatabase(connectionInfo);            
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyDb;Integrated Security=True;";            
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-
-        public void MigrateDatabase(string connectionString, TextWriter output = null)
-        {
-            var runner = new RunnerContext(new TextWriterAnnouncer(output ?? Console.Out))
+            var runner = new RunnerContext(new TextWriterAnnouncer(Console.Out))
             {
                 Database = "SqlServer",
                 Connection = connectionString,
